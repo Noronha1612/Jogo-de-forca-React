d398acbbbb0f03351e2fd598449ad86af901d513
Gabriel Noronha Cavalcante Pessoa <56375499+Noronha1612@users.noreply.github.com>
2020-03-19 00:49:21 -0300

    ReadMe Update

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-Classic Hangman Game made with React.
+This project has been created by Create-React-App(https://github.com/facebook/create-react-app);
 
-To execute the code in development server open your console and run the following commands at the project's directory:
+Classic Hangman Game.
 
-npm install or yarn install;
+To run this project on a develepment server run the following commands in your prompt when the Project's directory is opened:
 
-npm start or yarn start;
+To install the main dependencies and be able to run the next command: "yarn install (or just "yarn")" (if you are using "Yarn") or "npm start" (if you are using node)
+
+to install Yarn follow the step-by-step in this link: "https://classic.yarnpkg.com/"
+
+to install NodeJS just download and install the LTS(Long Term Support) on their website: "https://nodejs.org/"
+
--- src/components/interfaceComponents/SelectCateg.js
+++ src/components/interfaceComponents/SelectCateg.js
@@ -1,20 +1,24 @@
 import React from "react";
 import $ from "jquery";
 
+function CategSelBtn(props) {
 
-export default function SelectCateg(props) {
+  function selectCateg(categ) {
 
-    function selectCateg(categ) {
-  
-      for (let key = 1; key <= 8; key++) {
-        if ( $(`#categ${key}`).hasClass("categ-selected") ) $(`#categ${key}`).removeClass("categ-selected")
-      }
-  
-      $(`#categ${categ}`).addClass("categ-selected")
-  
-      if ( $("#confBtn-Box").hasClass("hidden") ) $("#confBtn-Box").removeClass("hidden")
+    for (let key = 1; key <= 8; key++) {
+      if ( $(`#categ${key}`).hasClass("categ-selected") ) $(`#categ${key}`).removeClass("categ-selected")
     }
 
+    $(`#categ${categ}`).addClass("categ-selected")
+
+    if ( $("#confBtn-Box").hasClass("hidden") ) $("#confBtn-Box").removeClass("hidden")
+  }
+
+  return <li id={ `categ${props.Iden}` } className="categ" onClick={ () => { selectCateg(props.Iden) } } >{props.children}</li>
+}
+
+
+export default function SelectCateg(props) {
     return (
         <section>
 
@@ -24,17 +28,17 @@ export default function SelectCateg(props) {
 
             <div className="categs-box">
               <ul className="categ-line">
-                <li id="categ1" className="categ" onClick={() => { selectCateg(1) }}>Animais</li>
-                <li id="categ2" className="categ" onClick={() => { selectCateg(2) }}>Cidades</li>
-                <li id="categ3" className="categ" onClick={() => { selectCateg(3) }}>Comidas</li>
-                <li id="categ4" className="categ" onClick={() => { selectCateg(4) }}>Eletrônicos</li>
+                <CategSelBtn Iden={1}>Animais</CategSelBtn>
+                <CategSelBtn Iden={2} >Cidades</CategSelBtn>
+                <CategSelBtn Iden={3}>Comidas</CategSelBtn>
+                <CategSelBtn Iden={4}>Eletrônicos</CategSelBtn>
               </ul>
 
               <ul className="categ-line">
-                <li id="categ5" className="categ" onClick={() => { selectCateg(5) }}>Esportes</li>
-                <li id="categ6" className="categ" onClick={() => { selectCateg(6) }}>Video-games</li>
-                <li id="categ7" className="categ" onClick={() => { selectCateg(7) }}>Pokémons</li>
-                <li id="categ8" className="categ" onClick={() => { selectCateg(8) }}>Todas as categorias</li>
+                <CategSelBtn Iden={5}>Esportes</CategSelBtn>
+                <CategSelBtn Iden={6}>Video-games</CategSelBtn>
+                <CategSelBtn Iden={7}>Pokémons</CategSelBtn>
+                <CategSelBtn Iden={8}>Todas as categorias</CategSelBtn>
               </ul>
             </div>
 
